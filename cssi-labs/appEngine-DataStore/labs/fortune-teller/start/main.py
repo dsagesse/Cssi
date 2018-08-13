8d223f55af98e42b2d19248044f6dcdb27a7dfd9
dsagesse <dbsagesse1@gmail.com>
2018-08-13 15:37:42 -0400

    Fortune

--- cssi-labs/appEngine-DataStore/labs/fortune-teller/start/main.py
+++ cssi-labs/appEngine-DataStore/labs/fortune-teller/start/main.py
@@ -33,24 +33,30 @@
 import webapp2
 import os
 import random
+import jinja2
 
 
 def get_fortune():
     #add a list of fortunes to the empty fortune_list array
-    fortune_list=['fortune1', 'fortune2']
+    fortune_list=['You well have great success soon', 'Something postive is coming your way']
     #use the random library to return a random element from the array
-    random_fortune =
+    random_fortune = fortune_list[random.randint(0,1)]
     return(random_fortune)
 
 
 #remember, you can get this by searching for jinja2 google app engine
-jinja_current_directory = "insert jinja2 environment variable here"
+jinja_current_directory = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class FortuneHandler(webapp2.RequestHandler):
     def get(self):
+        result_template= jinja_current_directory.get_template("templates/fortune-results.html")
         # In part 2, instead of returning this string,
         # make a function call that returns a random fortune.
-        self.response.write('a response from the FortuneHandler')
+        #get_fortune()
+        self.response.write(result_template.render())
     #add a post method
     #def post(self):
 
@@ -58,10 +64,15 @@ class HelloHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello World. Welcome to the root route of my app')
 
+class GoodbyeHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('My response is Goodbye World')
+
 #the route mapping
 app = webapp2.WSGIApplication([
     #this line routes the main url ('/')  - also know as
     #the root route - to the Fortune Handler
     ('/', HelloHandler),
-    ('/predict', FortuneHandler) #maps '/predict' to the FortuneHandler
+    ('/predict', FortuneHandler), #maps '/predict' to the FortuneHandler
+    ('/farewell', GoodbyeHandler),
 ], debug=True)
